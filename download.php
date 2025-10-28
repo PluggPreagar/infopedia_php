c398ce0f0e756580e8de237840fbe352bd0108b1
pluggPreagar <pluggPreagar@preisfrieden.de>
2025-10-28 09:24:20 +0100

    fix download - response not available any more

--- download.php
+++ download.php
@@ -40,6 +40,6 @@
     readfile($downloadFile);
     $data = "file=" . $downloadFile;
 
-    log_return( str_leng($response) . " bytes saved ( " . $data . ")"  );
+    log_return("downloaded: " . $data . ""  );
 
 ?>
